013114babaa1826888508a946d0a160a9ab08249
camocros <sernaarciniegas@gmail.com>
2026-06-16 21:24:41 -0600

    Update Swagger host to Render URL

--- swagger.js
+++ swagger.js
@@ -5,7 +5,8 @@ const doc = {
     title: 'Portfolio Builder API',
     description: 'API for CSE 341 Final Project Part 1',
   },
-  schemes: ['http', 'https'],
+  host: 'cse-341-w05.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
