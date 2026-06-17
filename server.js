970811e1f6ace16a268ae09ccdfbb7a611ffabc8
camocros <sernaarciniegas@gmail.com>
2026-06-16 21:31:20 -0600

    Disable browser caching for Swagger

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app
+  .use('/api-docs', (req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    res.setHeader('Surrogate-Control', 'no-store');
+    next();
+  })
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
     customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css'
   }))
